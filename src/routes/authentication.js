f2fbb0b1132fcb4adb976bd00ddb34f0f3c35217
Jamie Gee <jamie.gee@spongeuk.com>
2015-10-06 14:49:50 +0100

    Fixed route

--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -67,7 +67,7 @@
 			router.post('/logout', Auth.middleware.applyCSRF, controllers.authentication.logout);
 
             router.post('/registerMany', Auth.middleware.validateRequestSource, controllers.authentication.registerMany);
-            router.post('/loginRedirect', Auth.middleware.validateRequestSource, controllers.authentication.loginToPost);
+            router.post('/loginRedirect', Auth.middleware.validateRequestSource, controllers.authentication.login);
 
 			hotswap.replace('auth', router);
 			if (typeof callback === 'function') {
