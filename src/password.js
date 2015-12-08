3e1fcd5e166a812a2b9e079469228c5677713e19
Jamie Gee <jamie.gee@spongeuk.com>
2015-12-08 11:23:40 +0000

    Updated password compare

--- src/password.js
+++ src/password.js
@@ -1,4 +1,5 @@
 'use strict';
+var bcrypt = require('bcrypt');
 
 (function(module) {
 	var fork = require('child_process').fork;
@@ -8,26 +9,23 @@
 	};
 
 	module.compare = function(password, hash, callback) {
-		forkChild({type: 'compare', password: password, hash: hash}, callback);
+		// forkChild({type: 'compare', password: password, hash: hash}, callback);
+        bcrypt.compare(password, hash, callback);
 	};
 
 	function forkChild(message, callback) {
-        console.log(new Date().toTimeString().split(" ")[0], " FORK CHILD");
 		var child = fork('./bcrypt', {
 				silent: true
 			});
 
-        console.log(new Date().toTimeString().split(" ")[0], " CHILD.ON");
 		child.on('message', function(msg) {
 			if (msg.err) {
 				return callback(new Error(msg.err));
 			}
 
-            console.log(new Date().toTimeString().split(" ")[0], " PASSWORD CALLING BACK");
 			callback(null, msg.result);
 		});
 
-        console.log(new Date().toTimeString().split(" ")[0], " CHILD.SEND");
 		child.send(message);
 	}
 
