0d522b513461ed15ac7afe0485e944015186a817
Jamie Gee <jamie.gee@spongeuk.com>
2016-01-21 11:53:52 +0000

    Updated backup script

--- backup_data.sh
+++ backup_data.sh
@@ -5,3 +5,6 @@ filepath=$path$date
 source="/var/lib/redis/redis.rdb"
 echo "Copying $source to $filepath"
 sudo cp $source $filepath
+cd $path
+echo "Clearing old backups"
+ls -tp | grep -v '/'| tail -n +8 | xargs -I {} rm -- {}
